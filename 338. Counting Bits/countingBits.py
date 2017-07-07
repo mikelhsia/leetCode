c04743b1bac841701d7a999b6caca7323e0b91a3
mikelhsia <mikelhsia@hotmail.com>
2017-07-07 15:35:33 +0800

    Update

--- 338. Counting Bits/countingBits.py
+++ 338. Counting Bits/countingBits.py
@@ -4,9 +4,18 @@
 import math
 
 num = 5
-alist = [0]
+alist = [0] * (num + 1)
 
-for x in range(1,num):
-	print math.log(x, 2)
+
+for x in range(1,num + 1):
+	print x
+	print '**'
+	if int(math.log(x,2)) == math.log(x,2):
+		alist[x] = 1
+	else:
+		while (x / 2) >= 1:
+			print x
+			x = x/2 + x%2
+	print '*************'
 
 print alist
